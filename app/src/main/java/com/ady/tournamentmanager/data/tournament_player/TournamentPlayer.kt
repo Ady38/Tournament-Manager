cec2e098a8001ab0a5e304ecdc4f0a825e570823
Ady38 <chobot.adam.2004@gmail.com>
2025-05-31 10:44:29 +0200

    deleteed a general player database, altered the TournamentPlayer database

--- app/src/main/java/com/ady/tournamentmanager/data/tournament_player/TournamentPlayer.kt
+++ app/src/main/java/com/ady/tournamentmanager/data/tournament_player/TournamentPlayer.kt
@@ -2,7 +2,6 @@ package com.ady.tournamentmanager.data.tournament_player
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.ady.tournamentmanager.data.player.Player
 import com.ady.tournamentmanager.data.tournament.Tournament
 
 @Entity(tableName = "tournament_players")
@@ -10,6 +9,7 @@ data class TournamentPlayer (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val tournament: Tournament,
-    val player: Player,
+    val name: String,
+    val surname: String,
     val points: Double
 )
